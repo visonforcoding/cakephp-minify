7de4897ee44efb2ee64b5897d1ed0303f23228d7
visonforcoding <caowenpeng1990@126.com>
2017-03-04 14:14:25 +0800

    add new func

--- src/View/Helper/MinifyHelper.php
+++ src/View/Helper/MinifyHelper.php
@@ -4,7 +4,7 @@ namespace Cakeminify\View\Helper;
 
 use Cake\View\Helper;
 use Cake\View\View;
-
+use MatthiasMullie\Minify;
 
 /**
  * Minify helper
@@ -20,7 +20,7 @@ class MinifyHelper extends Helper {
     protected $_defaultConfig = [];
     public $helpers = ['Html'];
 
-    public function generateAsset() {
+    public function generateAsset($config = null) {
         $debug = \Cake\Core\Configure::read('debug');
         $css_source = \Cake\Core\Configure::read('mincss.source');
         $js_source = \Cake\Core\Configure::read('minjs.source');
@@ -30,11 +30,69 @@ class MinifyHelper extends Helper {
         if ($debug) {
             $output .= $this->Html->css($css_source);
             $output .= $this->Html->script($js_source);
-        }else{
+        } else {
             $output .= $this->Html->css($css_desc);
             $output .= $this->Html->script($js_desc);
         }
         return $output;
+    }
+
+    /**
+     * 输出css
+     * @param array $css
+     * @param string $output
+     * @param bool $minify 是否压缩
+     */
+    public function outputCss($css, $output, $minify = true) {
+        $debug = \Cake\Core\Configure::read('debug');
+        if ($debug) {
+            return $this->Html->css($css);
+        }
+        if (file_exists(WWW_ROOT . substr($output, 1))) {
+            return $this->Html->css($output);
+        }
+        if (!$minify) {
+            foreach ($css as $v) {
+                file_put_contents(WWW_ROOT . substr($output, 1), file_get_contents(WWW_ROOT . substr($v, 1)), FILE_APPEND);
+            }
+            return $this->Html->css($output);
+        }
+        $minifier = new Minify\CSS();
+        foreach ($css as $v) {
+            $minifier->add(WWW_ROOT . substr($v, 1));
+        }
+        $minifier->minify(WWW_ROOT . substr($output, 1));
+        return $this->Html->css($output);
+    }
+
+    /**
+     * 输出js
+     * @param array $js
+     * @param string $output
+     * @param bool $minify Description
+     * @return type
+     */
+    public function outputJs($js, $output, $minify = true) {
+        $debug = \Cake\Core\Configure::read('debug');
+        if ($debug) {
+            return $this->Html->script($js);
+        }
+        if (file_exists(WWW_ROOT . substr($output, 1))) {
+            return $this->Html->script($output);
+        }
+        if (!$minify) {
+            foreach ($js as $v) {
+                file_put_contents(WWW_ROOT . substr($output, 1), file_get_contents(WWW_ROOT . substr($v, 1)), FILE_APPEND);
+            }
+            return $this->Html->script($output);
+        }
+
+        $minifier = new Minify\JS();
+        foreach ($js as $v) {
+            $minifier->add(WWW_ROOT . substr($v, 1));
+        }
+        $minifier->minify(WWW_ROOT . substr($output, 1));
+        return $this->Html->script($output);
     }
 
 }
